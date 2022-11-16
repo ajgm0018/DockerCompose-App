c4efe680bc0e60f11cac7d9fc0c84f9b8565b087
ajgm0018 <ajgm0018.red.ujaen.es>
2022-11-16 11:43:51 +0100

    Prueba proxy 2

--- bighug-app/src/setupProxy.js
+++ bighug-app/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/app1',
+        '/app1/',
         createProxyMiddleware({
             target: 'http://localhost:8008',
             changeOrigin: true,
